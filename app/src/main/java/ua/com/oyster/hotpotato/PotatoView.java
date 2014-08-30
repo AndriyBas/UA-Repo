e78acf92fd87775bbc896ad8f2f63d19be798522
linkbe <serhii.onopriienko@gmail.com>
2014-08-31 02:37:09 +0300

    fix rotating

--- app/src/main/java/ua/com/oyster/hotpotato/PotatoView.java
+++ app/src/main/java/ua/com/oyster/hotpotato/PotatoView.java
@@ -1,5 +1,6 @@
 package ua.com.oyster.hotpotato;
 
+import android.app.AlertDialog;
 import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
@@ -8,6 +9,8 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Matrix;
 import android.graphics.Paint;
+import android.graphics.Rect;
+import android.util.DisplayMetrics;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -19,21 +22,32 @@ public class PotatoView extends View {
     private Paint backgrouPaint;
     private Potato mPotato;
     private Thread mDrawingThread;
+    private Rect mBorder;
 
-    private int downX, downY, upX, upY, dX, dY, directionX, directionY;
+    private Context mContext;
+
+    private int downX, downY, upX, upY, directionX, directionY;
     Matrix matrix = new Matrix();
 
     //This is huck!!
     private int startFlag = 0;
-    private int rotation = 5;
+    private int rotation = 0;
 
 
     public PotatoView(Context context) {
         super(context);
 
+        mContext = context;
+
         backgrouPaint = new Paint();
         backgrouPaint.setColor(Color.WHITE);
         backgrouPaint.setStyle(Paint.Style.FILL);
+
+
+        DisplayMetrics metrics = context.getResources().getDisplayMetrics();
+        int width = metrics.widthPixels;
+        int height = metrics.heightPixels;
+        mBorder = new Rect(0, 0, width + 300, height + 300);
 
 
         Resources res = this.getResources();
@@ -62,6 +76,8 @@ public class PotatoView extends View {
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
+
+                    isPotatoOnMap();
                 }
             }
         });
@@ -77,22 +93,19 @@ public class PotatoView extends View {
 
         //ToDo fix this huck
         if (startFlag == 0) {
-            mPotato.setX(this.getWidth() / 3);
-            mPotato.setY(this.getHeight() / 3);
+            mPotato.setX(this.getWidth() - 200);
+            mPotato.setY(this.getHeight() - 400);
             startFlag = 1;
 
         }
 
         matrix.postTranslate(-mPotato.getImage().getWidth() / 2, -mPotato.getImage().getHeight() / 2);
         matrix.postRotate(rotation);
-        rotation += 3;
+        rotation += 4;
         matrix.postTranslate(mPotato.getX() - mPotato.getImage().getWidth() / 2,
                 mPotato.getY() - mPotato.getImage().getHeight() / 2);
         canvas.drawBitmap(mPotato.getImage(), matrix, null);
         matrix.reset();
-
-//        canvas.drawBitmap(mPotato.getImage(), mPotato.getX() - mPotato.getImage().getWidth()  / 2,
-//                mPotato.getY() - mPotato.getImage().getHeight() / 2, null);
     }
 
 
@@ -114,52 +127,8 @@ public class PotatoView extends View {
             upX = (int) event.getX();
             upY = (int) event.getY();
 
-            dX = upX - downX;
-            dY = upY - downY;
-
-            if (dX > 0) {
-                if (dY > 0) {
-                    directionX = 10;
-                    directionY = 10;
-                } else {
-                    directionX = 10;
-                    directionY = -10;
-                }
-            } else {
-                if (dY > 0) {
-                    directionX = -10;
-                    directionY = 10;
-                } else {
-                    directionX = -10;
-                    directionY = -10;
-                }
-            }
-
-//
-//            if (Math.abs(downX - upX) > 100 || Math.abs(downY - upY) > 100) {
-//                new Thread(new Runnable() {
-//                    @Override
-//                    public void run() {
-//                        while (potatoOnMap()) {
-//                            try {
-//                                Thread.sleep(100);
-//                                PotatoView.this.post(new Runnable() {
-//                                    @Override
-//                                    public void run() {
-//                                        mPotato.setX(mPotato.getX() + directionX);
-//                                        mPotato.setY(mPotato.getY() + directionY);
-//                                        invalidate();
-//                                    }
-//                                });
-//
-//                            } catch (InterruptedException e) {
-//                                e.printStackTrace();
-//                            }
-//                        }
-//                    }
-//                }).start();
-//            }
-
+            directionX = (upX - downX) / 10;
+            directionY = (upY - downY) / 10;
 
         }
 
@@ -167,8 +136,38 @@ public class PotatoView extends View {
         return true;
     }
 
-    public boolean potatoOnMap() {
-        return Math.abs(mPotato.getX()) < this.getWidth() || Math.abs(mPotato.getY()) < this.getHeight();
+    public void isPotatoOnMap() {
+
+
+        if (!mBorder.contains(mPotato.getX(), mPotato.getY()))
+
+            if (!mDrawingThread.isInterrupted()) {
+                mDrawingThread.interrupt();
+
+                final AlertDialog.Builder alert = new AlertDialog.Builder(this.mContext);
+                alert.setTitle("Go Fuck Yourself!");
+                alert.setMessage("Game Over");
+                alert.setPositiveButton("OK", null);
+
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+
+                        PotatoView.this.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                alert.show();
+                                mPotato.setX(PotatoView.this.getWidth() - 200);
+                                mPotato.setY(PotatoView.this.getHeight() - 400);
+                                directionX = 0;
+                                directionY = 0;
+                                rotate();
+                            }
+                        });
+
+                    }
+                }).start();
+            }
     }
 
 }
